33aaa444dbdaa28668b312a656c2c06f644c13ec
abrahamin91 <abrahamin91@gmail.com>
2013-11-20 17:32:07 -0300

    modificacion de  migrate

--- db/migrate/001_alta_alumnos.rb
+++ db/migrate/001_alta_alumnos.rb
@@ -1,6 +1,6 @@
 migration 1, :alta_alumnos do
   up do
-    create_table :alumno do
+    create_table :alumnos do
       column :id, Integer, :serial => true
       column :nombre, DataMapper::Property::String, :length => 255
       column :apellido, DataMapper::Property::String, :length => 255
@@ -10,6 +10,6 @@ migration 1, :alta_alumnos do
   end
 
   down do
-    drop_table :alumno
+    drop_table :alumnos
   end
 end
